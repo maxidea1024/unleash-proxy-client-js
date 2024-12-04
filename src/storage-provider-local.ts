ccf2315c0991d9da5d9681c4e5e80e0968e6ef5a
maxidea1024@gmail.com <maxidea1024@gmail.com>
2024-12-04 14:00:14 +0900

    adjustments

--- src/storage-provider-local.ts
+++ src/storage-provider-local.ts
@@ -1,29 +1,29 @@
 import type IStorageProvider from './storage-provider';
 
 export default class LocalStorageProvider implements IStorageProvider {
-    private prefix: string;
+  private readonly prefix: string;
 
-    constructor(name = 'unleash:repository') {
-        this.prefix = name;
-    }
+  constructor(name = 'unleash:repository') {
+    this.prefix = name;
+  }
 
-    public async save(name: string, data: any) {
-        const repo = JSON.stringify(data);
-        const key = `${this.prefix}:${name}`;
-        try {
-            window.localStorage.setItem(key, repo);
-        } catch (error: unknown) {
-            console.error(error);
-        }
+  async save(name: string, data: any) {
+    const repo = JSON.stringify(data);
+    const key = `${this.prefix}:${name}`;
+    try {
+      window.localStorage.setItem(key, repo);
+    } catch (error: unknown) {
+      console.error(error);
     }
+  }
 
-    public get(name: string) {
-        try {
-            const key = `${this.prefix}:${name}`;
-            const data = window.localStorage.getItem(key);
-            return data ? JSON.parse(data) : undefined;
-        } catch (error: unknown) {
-            console.error(error);
-        }
+  get(name: string) {
+    try {
+      const key = `${this.prefix}:${name}`;
+      const data = window.localStorage.getItem(key);
+      return data ? JSON.parse(data) : undefined;
+    } catch (error: unknown) {
+      console.error(error);
     }
+  }
 }
